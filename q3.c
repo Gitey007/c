97c47690be5c55d5d72bd22be986af5d5d00490f
sahul prabhu <sahul.024b0101129@abes.ac.in>
2025-04-24 21:37:50 +0530

    question 3

---
+++ q3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    float r;
+    printf("give the radius of circle");
+    scanf("%f",&r);
+    float area=3.14*r*r;
+    float cir=3.14*2*r;
+    printf("area=%fcir=%f",area,cir);
+
+}
