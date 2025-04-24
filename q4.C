76c97d06616e88e8663a2a913a0e22c25b6afbcc
sahul prabhu <sahul.024b0101129@abes.ac.in>
2025-04-24 22:21:57 +0530

    question 4

---
+++ q4.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    float c,f;
+    printf("write the temprature in celcius");
+    scanf("%f",&c);
+    f=1.8*c+32;
+    printf("temprature in f=%f",f);
+    
+
+
+}
