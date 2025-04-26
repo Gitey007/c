84e5c8b9f8f2b6d1e5dd0b65976a5fd3ef312210
sahul prabhu <sahul.024b0101129@abes.ac.in>
2025-04-26 12:29:36 +0530

    test

--- marks.c
+++ marks.c
@@ -5,4 +5,4 @@ int main(){
     scanf("%d%d%d%d%d",&n1,&n2,&n3,&n4,&n4);
     int p=(n1+n2+n3+n4+n5)/5;
     (p>=90)?printf("grade is A"):(p>=80)?printf("grade is B"):(p>=70)?printf("grade is C"):(p>=60)?printf("grade is D"):printf("grade is E");
-}
+    }
