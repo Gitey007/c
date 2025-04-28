7f1775391f83ae1ce9e773718295bab72069ac41
sahul prabhu <sahul.024b0101129@abes.ac.in>
2025-04-28 22:54:54 +0530

    upper case AND LOWER CASE

---
+++ ul.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    char ch;
+    printf(" Enter the charcter :\n");
+    scanf("%c",&ch);
+    if(ch>='a' && ch<='z'){
+    printf(" character is lower case");
+    } 
+    else if (ch>='A' && ch<='Z'){
+        printf(" character is upper case");
+    }
+    else{
+        printf(" not a alphabet");
+    }
+    return 0;
+
+}
