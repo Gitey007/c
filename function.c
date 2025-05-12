080c0b8e3f39116eeb42d68d586514507e3fc0b0
sahul prabhu <sahul.024b0101129@abes.ac.in>
2025-05-12 19:01:59 +0530

    function basic definition

---
+++ function.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+//  function declaration/prototype
+void printhello();  
+int main(){
+    // functiom call
+    printhello();
+    printhello();
+    printhello();
+}
+// function definition
+void printhello(){
+    printf("hello\n");
+}
