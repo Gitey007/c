112328efadd7f1d6ebe7985e8ac8551143a5fc16
sahul prabhu <sahul.024b0101129@abes.ac.in>
2025-05-12 19:10:52 +0530

    q27

---
+++ q27.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void hello();
+void goodbye();
+int main (){
+    goodbye();
+    hello();
+}
+void hello(){
+    printf("hello\n");
+}
+void goodbye(){
+    printf("goodbye\n");
+}
+
+
