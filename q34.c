b6ba8a17a8e7667d787b23c76e545401ab5ecc3b
sahul prabhu <sahul.024b0101129@abes.ac.in>
2025-05-18 15:14:41 +0530

    fibonacci of number

---
+++ q34.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int fibo(int n);
+int main(){
+    int n;
+    printf("enter the value of n:\t");
+    scanf("%d",&n);
+    printf("fibo of %d is %d",n,fibo(n));
+}
+int fibo(int n){
+    if(n==0){
+        return 0;
+    }if(n==1){
+        return 1;
+    }
+    int fibonm1=fibo(n-1);
+    int fibonm2=fibo(n-2);
+    int fibo=fibonm1+fibonm2;
+    return fibo;
+}
