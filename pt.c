74f6aabdf22a3be7189ba9f3c7006034134452af
sahul prabhu <sahul.024b0101129@abes.ac.in>
2025-05-25 16:48:18 +0530

    hii

---
+++ pt.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n,i;
+    printf("enter the number of elements in the array\n");
+    scanf("%d",n);
+    for(i=0;i<=n;i++){
+        printf("\n");
+    
+    for(int j=1;j<=i;j++){
+    printf("%d",i);
+}}
+    
+}
